b4f6fdb42a7671c7891bf1d93de91259febb9e9e
vishruthp <20371189+Vishruthp@users.noreply.github.com>
2025-07-19 11:04:29 +0530

    teds

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    basePath: "/portfolio",
     output: "export",
-    reactStrictMode: true,
 };
 
 export default nextConfig;
