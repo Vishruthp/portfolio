c3b1517f7535565ba30edb53b59d96abaa05c071
vishruthp <20371189+Vishruthp@users.noreply.github.com>
2025-07-20 19:37:54 +0530

    changes to wordings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h2> Hello this is a test again</h2>
+      <h2> Now does it work with quick updates </h2>
     </div>
   );
 }
