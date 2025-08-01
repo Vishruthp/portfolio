28b53d5d7421ce051830428cfc1b5d90045717d2
vishruthp <20371189+Vishruthp@users.noreply.github.com>
2025-08-01 20:42:44 +0530

    some changes

--- src/common_components/Skill/Skill.tsx
+++ src/common_components/Skill/Skill.tsx
@@ -8,19 +8,19 @@ type SkillProps = {
 };
 
 export enum SkillType {
-  Github = "/github.svg",
-  Angular = "/angular.png",
-  Arduino = "/adruino.png",
-  Couch = "/couch.png",
-  Docker = "/docker.png",
-  React = "/react.png",
-  DotNet = "/net.png",
-  NextJs = "/next.svg",
-  MongoDB = "/mongodb.png",
-  NodeJs = "/nodejs.png",
-  Kubernetes = "/kubernetes.png",
-  Postgres = "/postgresql.svg",
-  Redis = "/redis.png",
+  Github = "/assets/github.svg",
+  Angular = "/assets/angular.png",
+  Arduino = "/assets/adruino.png",
+  Couch = "/assets/couch.png",
+  Docker = "/assets/docker.png",
+  React = "/assets/react.png",
+  DotNet = "/assets/net.png",
+  NextJs = "/assets/next.svg",
+  MongoDB = "/assets/mongodb.png",
+  NodeJs = "/assets/nodejs.png",
+  Kubernetes = "/assets/kubernetes.png",
+  Postgres = "/assets/postgresql.svg",
+  Redis = "/assets/redis.png",
 }
 
 function Skill({ size, skillType }: SkillProps) {
